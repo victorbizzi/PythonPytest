69da2e138f1fc21c0179f09d09ad652aca42e174
victorabgdm <136820074+victorabgdm@users.noreply.github.com>
2024-03-04 16:56:38 +0000

    improving the robot

--- utilities/globalVars.py
+++ utilities/globalVars.py
@@ -1,6 +1,12 @@
 # pip install webdriver_manager
 # pip install verbose
 # pip install pytest_sugar
+#
+# pip install pulsar-client
+# pip install behave
+# pip install --upgrade pip
+# pip install --upgrade setuptools
+#
 
 class globalVars:
     base_url = "https://www.saucedemo.com/"
